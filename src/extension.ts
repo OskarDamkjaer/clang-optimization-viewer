287f8fe956a13e2eb91e2c1194a3ed109234adba
Oskar Damkjaer <oskar.damkjaer@gmai.com>
2020-02-06 15:21:16 +0100

    a

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,34 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import { exec } from "child_process";
+const clangbin = "$HOME/thesis-llvm/build/bin/clang";
+const flags = "-c -o /dev/null -O3 -foptimization-record-file=$1";
+const clang = "clang";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "opt-info" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     "extension.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello yas!");
-    }
+    async () =>
+      new Promise(r =>
+        exec(
+          `${clang} ${flags} ${
+            vscode.window.activeTextEditor!.document.fileName
+          }`,
+          (error, stdout, stderr) => {
+            if (error) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            // todo skaffa en readabl3e steam att ge till exec eler dyl och ta bort promise
+            console.log(`stdout: ${stdout}`);
+            console.error(`stderr: ${stderr}`);
+            vscode.window.showInformationMessage("a");
+            r();
+          }
+        )
+      )
   );
 
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
---
+++ test/code.c
@@ -0,0 +1,7 @@
+int main() {
+  int sum[1000] = {1, 2, 3, 4};
+
+  for (int i = 1; i < 1000; i++) {
+    sum[0] += sum[i - 1];
+  }
+}
